9cca1cbb8ba6433fdf120951ad6590c299d0ae91
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-04-20 14:43:47 -0400

    Try to replicate plastic labs' bloombot

--- services/course_materials.py
+++
@@ -1,9 +0,0 @@
-from fastapi import HTTPException
-from supabase import Client
-
-TABLE_NAME = "course_materials"
-
-
-def get_course_materials(db: Client, course_id: int):
-    res = db.table(TABLE_NAME).select("*").eq("course_id", course_id).execute().data
-    return res
